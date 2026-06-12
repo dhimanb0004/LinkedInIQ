ad95e7285541ef32806b047a477ce7132207aeb5
Dhiman Bharadwaj <dhimanb0004@gmail.com>
2026-06-12 20:38:47 +0530

    Day 1: Environment setup and Hello World

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="LinkedIQ", page_icon="💡")
+
+st.title("Welcome to LinkedIQ")
+st.write("If you can see this, Day 1 environment setup is a success!")
